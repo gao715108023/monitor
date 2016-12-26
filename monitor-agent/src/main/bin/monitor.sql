5853b450b00aba28c09521f8680a643505012f6e
高传俊 <gaochuanjun@ppdai.com>
2016-12-26 18:59:29 +0800

    add log

--- monitor-agent/src/main/bin/monitor.sql
+++ monitor-agent/src/main/bin/monitor.sql
@@ -58,3 +58,66 @@ CREATE TABLE `io_monitor` (
   AUTO_INCREMENT = 1
   DEFAULT CHARSET = utf8mb4
   ROW_FORMAT = COMPACT;
+
+-- ----------------------------
+--  Table structure for `netstat_monitor`
+-- ----------------------------
+
+DROP TABLE
+IF EXISTS `netstat_monitor`;
+
+CREATE TABLE `netstat_monitor` (
+  `id`          BIGINT(20)   NOT NULL AUTO_INCREMENT,
+  `ip`          VARCHAR(128) NOT NULL
+  COMMENT '服务器ip地址',
+  `port`        INT          NOT NULL DEFAULT 0
+  COMMENT '被监控的端口号',
+  `total`       INT          NOT NULL DEFAULT 0
+  COMMENT '总连接数',
+  `established` INT          NOT NULL DEFAULT 0
+  COMMENT '状态为ESTABLISHED的连接数',
+  `time_wait`   INT          NOT NULL DEFAULT 0
+  COMMENT '状态为TIME_WAIT的连接数',
+  `fin_wait2`   INT          NOT NULL DEFAULT 0
+  COMMENT '状态为FIN_WAIT2的连接数',
+  `gmt_create`  DATETIME              DEFAULT NULL
+  COMMENT '创建时间',
+  PRIMARY KEY (`id`)
+)
+  ENGINE = INNODB
+  AUTO_INCREMENT = 1
+  DEFAULT CHARSET = utf8mb4
+  ROW_FORMAT = COMPACT;
+
+-- ----------------------------
+--  Table structure for `traffic_monitor`
+-- ----------------------------
+DROP TABLE
+IF EXISTS `traffic_monitor`;
+
+CREATE TABLE `traffic_monitor` (
+  `id`                BIGINT(20)   NOT NULL AUTO_INCREMENT,
+  `ip`                VARCHAR(128) NOT NULL
+  COMMENT '服务器ip地址',
+  `network_card_name` VARCHAR(128) NOT NULL
+  COMMENT '被监控的网卡名称',
+  `receive_traffic`   FLOAT        NOT NULL DEFAULT 0
+  COMMENT '流入带宽/秒',
+  `receive_packets`   FLOAT        NOT NULL DEFAULT 0
+  COMMENT '流入正确的包量/秒',
+  `receive_errs`      FLOAT        NOT NULL DEFAULT 0
+  COMMENT '流入错误的包量/秒',
+  `transmit_traffic`  FLOAT        NOT NULL DEFAULT 0
+  COMMENT '流出带宽/秒',
+  `transmit_packets`  FLOAT        NOT NULL DEFAULT 0
+  COMMENT '流出正确的包量/秒',
+  `transmit_errs`     FLOAT        NOT NULL DEFAULT 0
+  COMMENT '流出错误的包量/秒',
+  `gmt_create`        DATETIME              DEFAULT NULL
+  COMMENT '创建时间',
+  PRIMARY KEY (`id`)
+)
+  ENGINE = INNODB
+  AUTO_INCREMENT = 1
+  DEFAULT CHARSET = utf8mb4
+  ROW_FORMAT = COMPACT;
